b98540425a05af2cfee23fe8427011df38bfa1f1
sowweggod <rrrrr1111111111@inbox.ru>
2023-04-30 22:18:15 +0300

    ^-^

---
+++ Task 2(2)/Task 2(2)/Task 2(2).cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+int height(Node* node) {
+    if (!node) {
+        return 0;
+    }
+    else {
+        int leftHeight = height(node->left);
+        int rightHeight = height(node->right);
+        if (leftHeight > rightHeight) {
+            return (leftHeight + 1);
+        }
+        else {
+            return (rightHeight + 1);
+        }
+    }
+}
+
+int balanceFactor(Node* node) {
+    if (!node) {
+        return 0;
+    }
+    else {
+        int leftHeight = height(node->left);
+        int rightHeight = height(node->right);
+        return (leftHeight - rightHeight);
+    }
+}
+
+Node* newNode(int data) {
+    Node* node = new Node();
+    node->data = data;
+    node->left = nullptr;
+    node->right = nullptr;
+    return node;
+}
+
+Node* insert(Node* node, int data) {
+    if (!node) {
+        return newNode(data);
+    }
+    else {
+        if (data <= node->data) {
+            node->left = insert(node->left, data);
+        }
+        else {
+            node->right = insert(node->right, data);
+        }
+        return node;
+    }
+}
+
+void printBalanceFactors(Node* node) {
+    if (node) {
+        printBalanceFactors(node->left);
+        std::cout << "Balance factor for node " << node->data << " is " << balanceFactor(node) << std::endl;
+        printBalanceFactors(node->right);
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 4);
+    insert(root, 2);
+    insert(root, 1);
+    insert(root, 3);
+    insert(root, 6);
+    insert(root, 5);
+    insert(root, 7);
+    printBalanceFactors(root);
+    return 0;
+}
---
+++ Task 3(2)/Task 3(2)/Task 3(2).cpp
@@ -0,0 +1,95 @@
+﻿
+Node* newNode(int data) {
+    Node* node = new Node();
+    node->data = data;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->height = 1;
+    return node;
+}
+
+Node* rightRotate(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
+    x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
+
+    return x;
+}
+
+Node* leftRotate(Node* x) {
+    Node* y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = std::max(getHeight(x->left), getHeight(x->right)) + 1;
+    y->height = std::max(getHeight(y->left), getHeight(y->right)) + 1;
+
+    return y;
+}
+
+Node* insert(Node* node, int data) {
+    if (!node) {
+        return newNode(data);
+    }
+    else if (data < node->data) {
+        node->left = insert(node->left, data);
+    }
+    else {
+        node->right = insert(node->right, data);
+    }
+
+    node->height = std::max(getHeight(node->left), getHeight(node->right)) + 1;
+
+    int balance = getBalance(node);
+
+    if (balance > 1 && data < node->left->data) {
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && data > node->right->data) {
+        return leftRotate(node);
+    }
+
+    if (balance > 1 && data > node->left->data) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && data < node->right->data) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+void preOrder(Node* node) {
+    if (node) {
+        std::cout << node->data << " ";
+        preOrder(node->left);
+        preOrder(node->right);
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+
+    std::cout << "Preorder traversal of the AVL tree is: ";
+    preOrder(root);
+    std::cout << std::endl;
+
+    return 0;
+}
